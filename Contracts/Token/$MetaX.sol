b4e6fac0195bd6fc771039f5952e849eafd1fbc3
MetaX <129074884+MetaX-Social@users.noreply.github.com>
2023-08-31 16:04:49 +0900

    Update $MetaX.sol

--- Contracts/Token/$MetaX.sol
+++ Contracts/Token/$MetaX.sol
@@ -3,136 +3,139 @@ pragma solidity ^0.8.18;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "../Interface/IMetaX.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
-import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract $MetaX is ERC20, ERC20Permit, Ownable, AccessControl, ReentrancyGuard {
+contract $MetaX is ERC20, AccessControl, Ownable {
 
 /** Roles **/
     bytes32 public constant Admin = keccak256("Admin");
 
     bytes32 public constant Burner = keccak256("Burner");
 
-    constructor() ERC20("MetaX", "MetaX") ERC20Permit("MetaX") {
+    constructor(
+        uint256 _T0
+    ) ERC20("MetaX", "MetaX") {
+        T0 = _T0;
         _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _grantRole(Admin, msg.sender);
     }
 
 /** Token Allocation **/
 
-    uint256 public immutable Max = 20000000000 ether;
-
+    /* Vaults by MetaX Smart Contracts */
     address[] public Vaults = [
-        0x5536d1ACd0aF249e087a582db479Ba768F74995D, /* Social Mining 40% */
-        0x42B3B01Af349CC744971EaF3FA781D2C8eA8bf5C, /* Builder Incentives 5% */
-        0x0000000000000000000000000000000000000000, /* Marketing Expense 10% */
-        0x0000000000000000000000000000000000000000, /* Treasure 10% */
-        0x0000000000000000000000000000000000000000, /* Team Reserved 15% */
-        0x0000000000000000000000000000000000000000, /* Advisors 5% */
-        0x0000000000000000000000000000000000000000  /* Investors 15% */
+        0xB5ec8e0b6C8f20ba344CD3d0eaA890dF6D0Bf7c4, /* #0 Social Mining 40% */
+        0x6A3bB40902C6B25ce8cbeFcD1E717f39De064Cb7, /* #1 Builder Incentives 5% */
+        0x00056aFf95b8B46ecA059c622a891E095792Dd47, /* #2 Marketing Expense 10% */
+        0x5a49E4c0549A31bfD728fFe8577C81c87624760e, /* #3 Treasure 10% */
+        0xEeB56e83fc333c3555ff8b9e64973113Db36CeCf, /* #4 Team Reserved 15% */
+        0x35Ebdf5dF44CbCc6007CBB6E2Cfdc4D5dD1Db961, /* #5 Advisors 5% */
+        0x49B7c5Eb4Ee5b72F10D4B8E242dA7598aC0e657C  /* #6 Investors 15% */
     ];
 
     function setVaults(uint256 batch, address _Vault) public onlyOwner {
         Vaults[batch] = _Vault;
     }
 
+    /* Tokens Allocation */
+    uint256 public immutable Max = 20000000000 ether;
+
+    function maxSupply() external view returns (uint256) {
+        return Max - numberBurnt;
+    }
+
     uint256[] public Allocation = [
-        8000000000, /* Social Mining 40% */
-        1000000000, /* Builder Incentives 5% */
-        2000000000, /* Marketing Expense 10% */
-        2000000000, /* Treasure 10% */
-        3000000000, /* Team Reserved 15% */
-        1000000000, /* Advisors 5% */
-        3000000000  /* Investors 15% */
+        8000000000 ether, /* #0 Social Mining 40% */
+        1000000000 ether, /* #1 Builder Incentives 5% */
+        2000000000 ether, /* #2 Marketing Expense 10% */
+        2000000000 ether, /* #3 Treasure 10% */
+        3000000000 ether, /* #4 Team Reserved 15% */
+        1000000000 ether, /* #5 Advisors 5% */
+        3000000000 ether  /* #6 Investors 15% */
     ];
 
-    mapping (uint256 => bool) public alreadyMinted;
+    uint256[] public releaseIntervals = [
+          5479452 ether, /* #0 Social Mining Daily Release | Halve every 2 years */
+           684931 ether, /* #1 Builder Incentives Daily Release | Halve every 2 years */
+                0 ether, /* #2 Marketing Expense | No lock period */
+                0 ether, /* #3 Treasure | No lock period */
+         83333333 ether, /* #4 Team Reserved Monthly Release | 36-Months Linear Release */
+         27777777 ether, /* #5 Advisors Monthly Release | 36-Months Linear Release */
+        125000000 ether  /* #6 Investors Monthly Release | 24-Months Linear Release */
+    ];
 
-    function Mint(uint256 batch) public onlyOwner {
-        require(!alreadyMinted[batch], "$MetaX: Max 20 Billion.");
-        _mint(Vaults[batch], Allocation[batch] * 1 ether);
-        alreadyMinted[batch] = true;
+    uint256 public timeIntervals = 1 days; /* Release Intervals for Social Mining & Builder Incentives */
+
+    function setTimeIntervals (uint256 newTimeIntervals) public onlyOwner {
+        uint256 ratio = (newTimeIntervals * 1 ether) / timeIntervals;
+        releaseIntervals[0] = releaseIntervals[0] * ratio / 1 ether;
+        releaseIntervals[1] = releaseIntervals[1] * ratio / 1 ether;
+        timeIntervals = newTimeIntervals;
     }
 
-/** BlackHole SBT **/
-    address public BlackHole_Addr;
+    uint256[] public recentMinted = [
+        1693526400, /* #0 Social Mining Starts 2023/9/1 00:00 UTC */
+        1693526400, /* #1 Builder Incentives Starts 2023/9/1 00:00 UTC */
+                 0, /* #2 Marketing Expense */
+                 0, /* #3 Treasure */
+        1693526400, /* #4 Team Reserved Starts 2023/9/1 00:00 UTC */
+        1693526400, /* #5 Advisors Starts 2023/9/1 00:00 UTC */
+        1693526400  /* #6 Investors Starts 2023/9/1 00:00 UTC */
+    ];
 
-    IMetaX public BH;
+    uint256[] public alreadyMinted = [0, 0, 0, 0, 0, 0, 0];
 
-    function setBlackHole(address _BlackHole_Addr) public onlyOwner {
-        require(!BlackHole_Freeze, "$MetaX: BlackHole is frozen.");
-        BlackHole_Addr = _BlackHole_Addr;
-        BH = IMetaX(_BlackHole_Addr);
-    }
-
-    bool public BlackHole_Freeze;
-
-    function Freeze() public onlyOwner {
-        BlackHole_Freeze = true;
-    }
-
-/** Liquidity Release **/
-    function Liquidity() public view returns (bool) {
-        if (BH.totalSupply() >= 100) {
-            return true;
+    function Mint (uint256 batch, uint256 amount) public onlyOwner {
+        require(recentMinted[batch] < block.timestamp, "$MetaX: Please wait for the next release.");
+        require(alreadyMinted[batch] + releaseIntervals[batch] <= Allocation[batch], "$MetaX: All the tokens have been allocated.");
+        uint256 _amount = releaseIntervals[batch];
+        if (batch < 2) {
+            recentMinted[batch] += timeIntervals;
+        } else if (batch > 3) {
+            recentMinted[batch] += 30 days;
         } else {
-            return false;
+            require(amount + alreadyMinted[batch] <= Allocation[batch], "$MetaX: All the tokens have been allocated.");
+            _amount = amount;
         }
+        _mint(Vaults[batch], _amount);
+        alreadyMinted[batch] += _amount;
+        emit mintRecord(batch, _amount, block.timestamp);
     }
 
-    function checkVaults(address from) internal view returns (bool) {
-        bool withinVaults;
-        for (uint256 i=0; i<Vaults.length; i++) {
-            if (Vaults[i] == from) {
-                withinVaults = true;
-                break;
-            } else {
-                withinVaults = false;
-            }
-        }
-        return withinVaults;
+    event mintRecord (uint256 batch, uint256 amount, uint256 time);
+
+    /* Halve every 2 years */
+    uint256 public T0;
+
+    function Halve () public onlyOwner {
+        require(block.timestamp > T0 + 730 days, "$MetaX: Please wait till the next halving.");
+        releaseIntervals[0] /= 2;
+        releaseIntervals[1] /= 2;
+        T0 += 730 days;
     }
 
-    function checkConsumptions(address to) internal view returns (bool) {
-        bool withinConsumptions;
-        for (uint256 i=0; i<Consumptions.length; i++) {
-            if (Consumptions[i] == to) {
-                withinConsumptions = true;
-                break;
-            } else {
-                withinConsumptions = false;
-            }
-        }
-        return withinConsumptions;
-    }
+    /* Early Bird */
+    bool public alreadyEarlyBird;
 
-    function _transfer(address from, address to, uint256 amount) internal override {
-        if (from != address(0) && to != address(0)) {
-            if (!checkVaults(from) && !checkConsumptions(to)) {
-                require(Liquidity(), "$MetaX: Liquidity locked until 100 communities deployment.");
-            }
-        }
-        super._transfer(from, to, amount);
+    function Mint_EarlyBird (address earlyBirdUser_addr, uint256 earlyBirdUser, address earlyBirdBuilder_addr, uint256 earlyBirdBuilder) public onlyOwner {
+        require(!alreadyEarlyBird, "$MetaX: Early Bird Tokens have already been allocated");
+        alreadyMinted[0] += earlyBirdUser;
+        _mint(earlyBirdUser_addr, earlyBirdUser);
+        alreadyMinted[1] += earlyBirdBuilder;
+        _mint(earlyBirdBuilder_addr, earlyBirdBuilder);
+        alreadyEarlyBird = true;
     }
 
 /** Burn **/
-    address[] public Consumptions = [
-        0xC1f4862c56F76DdbA8D40438a58D678FF8736747, /* Vault */
-        0x0000000000000000000000000000000000000000,
-        0x0000000000000000000000000000000000000000
-    ];
+    uint256 public numberBurnt;
 
-    function updateConsumptions(uint256 batch, address _Consumptions) public onlyRole(Admin) {
-        if (batch < Consumptions.length) {
-            Consumptions[batch] = _Consumptions;
-        } else {
-            Consumptions.push(_Consumptions);
-        }
-    }
-
-    function Burn(address sender, uint256 amount) external onlyRole(Burner) {
+    function Burn (address sender, uint256 amount) external {
         require(balanceOf(sender) >= amount, "$MetaX: You don't have enough amount of $MetaX.");
         _burn(sender, amount);
+        numberBurnt += amount;
+        emit burnRecord(sender, amount, block.timestamp);
     }
+
+    event burnRecord(address smartContract, uint256 amount, uint256 time);
 }
